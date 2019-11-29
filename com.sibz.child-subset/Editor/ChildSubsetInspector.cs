918fc809276917f9dbeb8a8ec23e61511c5f2eb0
Steven Turner <steven@entityzero.com>
2019-11-30 08:33:06 +1100

    [Child Subset] Added initial files and structure

---
+++ com.sibz.child-subset/Editor/ChildSubsetInspector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using Sibz.UXMLList;
+
+namespace Sibz.ChildSubset
+{
+    [CustomEditor(typeof(ChildSubset))]
+    public class ChildSubsetInspector : Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            VisualElement root = new VisualElement();
+            ListVisualElement listVisualElement = new ListVisualElement
+            {
+                bindingPath = nameof(ChildSubset.Children),
+                Label = "Children",
+                HideAddButton = true,
+                HideDeleteAllButton = true,
+                HideReorderItemButtons = true,
+                HideDeleteItemButton = true,
+                DisablePropertyLabel = true
+            };
+            listVisualElement.Controls.ItemsSection.SetEnabled(false);
+            root.Add(listVisualElement);
+            root.Bind(serializedObject);
+            listVisualElement.BindProperty(serializedObject.FindProperty(nameof(ChildSubset.Children)));
+
+            return root;
+        }
+    }
+}
---
+++ com.sibz.child-subset/Runtime/ChildSubset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sibz.ChildSubset
+{
+    [AddComponentMenu("Sibz/Child Subset List")]
+    public class ChildSubset : MonoBehaviour
+    {
+        public List<GameObject> Children = new List<GameObject>();
+    }
+}
