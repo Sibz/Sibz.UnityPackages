236cd3305b658cf678e1a065d2dc12187227b66d
Steven Turner <12050358+Sibz@users.noreply.github.com>
2019-12-01 14:57:06 +0000

    [Child Subset] Refactored inspector

--- com.sibz.child-subset/Editor/ChildSubsetInspector.cs
+++ com.sibz.child-subset/Editor/ChildSubsetInspector.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEditor;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 using Sibz.UXMLList;
@@ -11,31 +8,44 @@ namespace Sibz.ChildSubset
     [CustomEditor(typeof(ChildSubset))]
     public class ChildSubsetInspector : Editor
     {
-        public virtual VisualTreeAsset MainContent => AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets("ChildSubsetInspector t:VisualTreeAsset")[0]));
-        public virtual VisualTreeAsset PingButton => AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets("ChildSubsetPingButton t:VisualTreeAsset")[0]));
+        protected virtual VisualTreeAsset MainContent => AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
+            AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets("ChildSubsetInspector t:VisualTreeAsset")[0]));
+
+        protected virtual VisualTreeAsset PingButton => AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
+            AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets("ChildSubsetPingButton t:VisualTreeAsset")[0]));
 
         public override VisualElement CreateInspectorGUI()
         {
             VisualElement root = new VisualElement();
+
             MainContent.CloneTree(root);
 
-            ListVisualElement listVisualElement = root.Q<ListVisualElement>();
-
-            listVisualElement.OnItemCreated += (item) =>
-            {
-                item.Q<PropertyField>()?.SetEnabled(false);
-                Button pingButton = PingButton.CloneTree().Q<Button>();
-                pingButton.clicked += () =>
-                 {
-                     EditorGUIUtility.PingObject(item.Q<ObjectField>().value);
-                 };                
-                pingButton.AddToClassList("sibz-child-subset-ping-button");
-                item.Add(pingButton);
-            };
+            AttachHandlersForItemCreation(root.Q<ListVisualElement>());
 
             root.Bind(serializedObject);
 
             return root;
+        }
+
+        private void AttachHandlersForItemCreation(ListVisualElement listVisualElement)
+        {
+            listVisualElement.OnItemCreated += DisablePropertyField;
+            listVisualElement.OnItemCreated += AddPingButtonWithEventHandler;
+        }
+
+        private static void DisablePropertyField(VisualElement itemSection)
+        {
+            itemSection.Q<PropertyField>()?.SetEnabled(false);
+        }
+
+        private void AddPingButtonWithEventHandler(VisualElement itemSection)
+        {
+            Button pingButton = PingButton.CloneTree().Q<Button>();
+
+            pingButton.clicked += () => { EditorGUIUtility.PingObject(itemSection.Q<ObjectField>().value); };
+
+            pingButton.AddToClassList("sibz-child-subset-ping-button");
+            itemSection.Add(pingButton);
         }
     }
 }
