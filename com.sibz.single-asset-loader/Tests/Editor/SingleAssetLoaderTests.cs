1ae3500426ef1755ca6aec3331eafe014bf448f6
Steven Turner <12050358+Sibz@users.noreply.github.com>
2019-12-07 16:26:01 +1100

    [Single Asset Loader] Added helper for loading 1st found asset by name and type

---
+++ com.sibz.single-asset-loader/Tests/Editor/SingleAssetLoaderTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Sibz.SingleAssetLoader.Tests
+{
+    public class SingleAssetLoaderTests
+    {
+        private const string TestAssetName = "TestGameObject";
+        private GameObject go;
+
+        [Test]
+        public void ShouldLoadAssetFromNameAndType()
+        {
+            Type type = typeof(GameObject);
+
+            go = PrefabUtility.SaveAsPrefabAsset(Object.Instantiate(new GameObject(TestAssetName)), $"assets/{TestAssetName}.prefab");
+
+            Object loadedAsset = SingleAssetLoader.Load(TestAssetName, type);
+
+            Assert.AreEqual(type, loadedAsset.GetType());
+            Assert.AreEqual(TestAssetName, loadedAsset.name);
+        }
+
+        [Test]
+        public void ShouldThrowIfAssetDoesNotExist()
+        {
+            Type type = typeof(GameObject);
+
+            bool errorThrown = false;
+            try
+            {
+                SingleAssetLoader.Load(TestAssetName, type);
+            }
+            catch
+            {
+                errorThrown = true;
+            }
+
+            Assert.IsTrue(errorThrown);
+        }
+
+        [Test]
+        public void GenericShouldLoadAssetFromNameAndType()
+        {
+            go = PrefabUtility.SaveAsPrefabAsset(Object.Instantiate(new GameObject(TestAssetName)), $"assets/{TestAssetName}.prefab");
+
+            GameObject loadedAsset = SingleAssetLoader.Load<GameObject>(TestAssetName);
+
+            Assert.IsNotNull(loadedAsset);
+            Assert.AreEqual(TestAssetName, loadedAsset.name);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (go != null)
+            {
+                AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(go));
+            }
+        }
+    }
+}
