9c9c320e1a6af6915007fdedbf04bd89fe808b69
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-03-16 22:55:15 +1100

    [List Element] #57 Added UxmlClass with some strings from options

---
+++ com.sibz.list-element/Editor/Internal/UxmlClassNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Sibz.ListElement.Internal
+{
+    public static class UxmlClassNames
+    {
+        public static string UseObjectField => "use-object-field";
+        public static string HideReorderButtons => "hide-reorder-buttons";
+        public static string HideRemoveButtons=> "hide-remove-buttons";
+        public static string HidePropertyLabel => "hide-field-label";
+    }
+}
