66c32161099185889bcbcf9fb464e07ad4afcd86
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-03-18 17:22:01 +1100

    [List Element] #45 #56 Added unit tests for ListElement

---
+++ com.sibz.list-element/Tests/Editor/Unit/ListElement/Constructor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEditor.UIElements;
+
+namespace Sibz.ListElement.Tests.Unit.ListElementTests
+{
+    public class Constructor
+    {
+        private ListElement listElement;
+        private SerializedProperty property;
+
+        [SetUp]
+        public void SetUp()
+        {
+            property = TestHelpers.GetProperty();
+        }
+
+        [Test]
+        public void WhenNoParams_ShouldNotInitialise()
+        {
+            listElement = new ListElement();
+            Assert.IsFalse(listElement.IsInitialised);
+        }
+
+        [Test]
+        public void WhenNoParams_ShouldNotBeEmpty()
+        {
+            listElement = new ListElement();
+            Assert.Greater(listElement.childCount, 0);
+        }
+
+        [Test]
+        public void WhenEmptySetTrue_ShouldBeEmpty()
+        {
+            listElement = new ListElement(true);
+            Assert.AreEqual(listElement.childCount, 0);
+        }
+
+        [Test]
+        public void WhenEmptySetFalse_ShouldNotBeEmpty()
+        {
+            listElement = new ListElement(false);
+            Assert.Greater(listElement.childCount, 0);
+        }
+
+        [Test]
+        public void WhenLabelSet_ShouldSetOption()
+        {
+            const string testString = "TEST123$$$";
+            listElement = new ListElement(property, testString);
+            Assert.AreEqual(testString, listElement.Options.Label);
+        }
+
+        [Test]
+        public void WhenPropertyIsNull_ShouldThrowArgumentNullException()
+        {
+            try
+            {
+                listElement = new ListElement(null);
+            }
+            catch (ArgumentNullException e)
+            {
+                Assert.AreEqual("property", e.ParamName);
+                return;
+            }
+
+            Assert.Fail($"{nameof(ArgumentNullException)} not thrown");
+        }
+
+        [Test]
+        public void WhenOptionsIsNull_ShouldNotThrowError()
+        {
+            ListElementOptions options = null;
+            listElement = new ListElement(property, options);
+        }
+
+        [Test]
+        public void WhenPropertyIsSet_ShouldContainControls()
+        {
+            listElement = new ListElement(property);
+            Assert.IsNotNull(listElement.Controls);
+        }
+        
+        [Test]
+        public void WhenPropertyIsSet_ShouldNotBeEmpty()
+        {
+            listElement = new ListElement(property);
+            Assert.Greater(listElement.childCount, 0);
+        }
+
+        [Test]
+        public void WhenPropertySet_ShouldBindProperty()
+        {
+             listElement = new ListElement(property);
+             Assert.AreEqual(property.propertyPath, listElement.bindingPath);
+        }
+        
+        [Test]
+        public void WhenPropertyBoundAfter_ShouldInitialise()
+        {
+            listElement = new ListElement();
+            listElement.BindProperty(property);
+            Assert.IsTrue(listElement.IsInitialised);
+        }
+    }
+}
