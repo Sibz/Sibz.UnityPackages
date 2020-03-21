a095576e055d9384784d062e4b1b100df4febbd5
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-03-21 13:19:55 +1100

    [List Element] #64 Added AddItem tests

---
+++ com.sibz.list-element/Tests/Editor/Acceptance/AddItem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Sibz.ListElement.Events;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Sibz.ListElement.Tests.Acceptance
+{
+    public class AddItem
+    {
+        private ListElement listElement;
+        private static SerializedProperty Property => TestHelpers.GetProperty();
+
+        private static readonly IEnumerable<ListOptions> WorkingOptionSet =
+            AcceptanceFixture.GetWorkingOptionSetExcl(nameof(ListOptions.EnableDeletions));
+
+        private Button Add => listElement.Controls.Add;
+
+        [Test]
+        public void ShouldAddItemToList([ValueSource(nameof(WorkingOptionSet))] ListOptions options, [Values(TestHelpers.CmdType.Click, TestHelpers.CmdType.Program)] TestHelpers.CmdType cmdType)
+        {
+            SerializedProperty property = Property;
+            listElement = new ListElement(property, options);
+            WindowFixture.RootElement.AddAndRemove(listElement, () =>
+            {
+                int initialSize = property.arraySize;
+                Add.SendEvent(new ClickEvent{target = Add});
+                listElement.SendEvent(new ListResetEvent {target = listElement});
+                Assert.AreEqual(initialSize+1, listElement.Controls.ItemsSection.childCount);
+            });
+        }
+
+        // TODO AddItem(object)
+        [Test]
+        public void ShouldAddObjectToList([ValueSource(nameof(WorkingOptionSet))] ListOptions options)
+        {
+
+        }
+
+        // TODO Enable Additions
+        [Test]
+        public void WhenDisabled_ShouldNotWork([ValueSource(nameof(WorkingOptionSet))] ListOptions options)
+        {
+
+        }
+
+        // TODO Enable Additions
+        [Test]
+        public void ShouldDisplayButtonAndFieldBasedOnOption([ValueSource(nameof(WorkingOptionSet))] ListOptions options, [Values(true,false)] bool option)
+        {
+
+        }
+    }
+}
